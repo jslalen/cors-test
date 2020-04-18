a59209a898aca9828317d217c9920cd282c659ba
jslalen <50656151+jslalen@users.noreply.github.com>
2020-04-18 18:44:06 +0530

    assignment4

---
+++ week4/harder/script.js
@@ -0,0 +1,12 @@
+(function () {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var prop in names) {
+        var firstLetter = names[prop].charAt(0);
+        if (firstLetter.toLowerCase() == 'j') {
+            byeSpeaker.speak(names[prop]);    
+        } else {
+            helloSpeaker.speak(names[prop]);
+        }
+    }
+}) ();
+
