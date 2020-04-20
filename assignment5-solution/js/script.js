932096ee618d3fbc62ed792f33676e6e6d431660
jslalen <50656151+jslalen@users.noreply.github.com>
2020-04-20 19:20:33 +0530

    check

--- assignment5-solution/js/script.js
+++ assignment5-solution/js/script.js
@@ -11,8 +11,8 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 (function (global) {
 
-var dc = {};
-
+    var dc = {};
+    //console.log("yes");
 var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
@@ -96,32 +96,32 @@ function buildAndShowHomeHTML (categories) {
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtml) {
+      function (homeHtml) {
 
-      // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
-      // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
-        // variable's name implies it expects.
-        var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-        //console.log(chosenCategoryShortName);
-      // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
-      // chosen category from STEP 2. Use existing insertProperty function for that purpose.
-      // Look through this code for an example of how to do use the insertProperty function.
-      // WARNING! You are inserting something that will have to result in a valid Javascript
-      // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
-      // being passed into the $dc.loadMenuItems function. Think about what that argument needs
-      // to look like. For example, a valid call would look something like this:
-      // $dc.loadMenuItems('L')
-      // Hint: you need to surround the chosen category short name with something before inserting
-        // it into the home html snippet.
-        
-        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "short_name", chosenCategoryShortName);
+          // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
+          // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
+          // variable's name implies it expects.
+          var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+          //console.log(chosenCategoryShortName);
+          // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
+          // chosen category from STEP 2. Use existing insertProperty function for that purpose.
+          // Look through this code for an example of how to do use the insertProperty function.
+          // WARNING! You are inserting something that will have to result in a valid Javascript
+          // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
+          // being passed into the $dc.loadMenuItems function. Think about what that argument needs
+          // to look like. For example, a valid call would look something like this:
+          // $dc.loadMenuItems('L')
+          // Hint: you need to surround the chosen category short name with something before inserting
+          // it into the home html snippet.
 
+          var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName",  chosenCategoryShortName );
 
+        //console.log(homeHtmlToInsertIntoMainPage);
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
         // ....
-        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+          insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
